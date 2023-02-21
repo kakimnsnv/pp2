c801fb39768fbfa3369ff98e9b6044134ebd5e17
Kakimbek Nyssanov <kakimbekn@gmail.com>
2023-02-21 11:14:58 +0600

    lab4

--- Labs/4/JSON/json.py
+++ Labs/4/JSON/json.py
@@ -1,8 +1,23 @@
 import json
-from json import loads, load
-f = open ('a.json', "r")
-  
-data = json.loads(f.read())
+from json import load
 
-print(data)
+# f = open(r'/Users/kakimbekn/Library/CloudStorage/OneDrive-АОКазахстанско-БританскийТехническийУниверситет/My Personal Data/docslocal/GitHub/pp2/Labs/4/JSON/a.json')
+# data = json.load(f)
+
+with open("/Users/kakimbekn/Library/CloudStorage/OneDrive-АОКазахстанско-БританскийТехническийУниверситет/My Personal Data/docslocal/GitHub/pp2/Labs/4/JSON/a.json", "r") as file:
+    data = json.load(file)
+
+
+print('''=======================================================================================
+DN                                                 Description           Speed    MTU" 
+-------------------------------------------------- --------------------  ------  ------''')
+
+imdata = data["imdata"]
+for i in imdata:
+        dn = i["l1PhysIf"]["attributes"]["dn"]
+        descr = i["l1PhysIf"]["attributes"]["descr"]
+        speed = i["l1PhysIf"]["attributes"]["speed"]
+        mtu = i["l1PhysIf"]["attributes"]["mtu"]
+        print("{0:51} {1:20} {2:8} {3:6}".format(dn,descr,speed,mtu))
+
 f.close()
